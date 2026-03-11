16799870781540843b80778eefa19190a2188a9b
noahj706 <noahj706@gmail.com>
2026-03-11 02:31:42 -0700

    im so sleepy, heres what I can muster

--- src/feature_plan_classes/plan_classes.cpp
+++ src/feature_plan_classes/plan_classes.cpp
@@ -1,7 +1,80 @@
 //fature_classes: defintions file
 //Programmer: Noah Julius
 
+#include "plan_classes.hpp"
+using std::string;
+
 class DailyDietPlan
 {
+//private(implicitly)
+int goalCalories;
+std::string name;
+std::string date;
 
+public:
+
+//constructor/desctructor
+DailyDietPlan(int newGoalCalories = 0, std::string newName = "Uknown", std::string newDate = "Uknown")//default constructor
+{
+    this->goalCalories = newGoalCalories;
+    this->name = newName;
+    this->date = newDate; 
+}
+DailyDietPlan(const DailyDietPlan& dietPlan)//copy constructor
+{
+    //only need a shallow copy(because no pointers need to be copied over), no definiton needed
+}
+~DailyDietPlan()//destructor
+{
+    //nothing on heap, leave blank
+}
+
+//setters
+bool setGoal(int newGoal)//returns true if valid goal ( >= 0 ) is passed
+{
+    if(newGoal < 0)
+    {
+        return false;
+    }
+    this->goalCalories = newGoal;
+    return true;
+}
+void setName(std::string newName)
+{
+    this->name = newName;
+}
+void setDate(std::string newDate)
+{
+    this->date = newDate;
+}
+
+//getters
+int getGoal()
+{
+    return goalCalories;
+}
+std::string getName()
+{
+    return name;
+}
+std::string getDate()
+{
+    return date;
+}
+
+//other member functions
+void printPlan()//prints outs plan for the day
+{
+
+}
+void editGoal()//prompts user for a new goal, loops until valid goal entered
+{
+    int newGoal = 0;
+    std::cout << "Enter your new caloric goal:\n -> " << std::endl;
+
+   do//loops until setGoal returns true (valid entry)
+   {
+        std::cin >> newGoal;
+   }while(!setGoal(newGoal));    
+}
 };
--- src/feature_plan_classes/plan_classes.hpp
+++ src/feature_plan_classes/plan_classes.hpp
@@ -2,6 +2,7 @@
 //Programmer: Noah Julius
 
 #include <iostream>
+#include <string>
 
 class DailyDietPlan
 {
@@ -11,12 +12,10 @@ int goalCalories;
 std::string name;
 std::string date;
 
-void printPlan();
-
 public:
 
 //constructor/desctructor
-DailyDietPlan(int goalCalories = 0, std::string name = "Uknown", std::string date = "Uknown");//default constructor
+DailyDietPlan(int newGoalCalories = 0, std::string newName = "Uknown", std::string newDate = "Uknown");//default constructor
 DailyDietPlan(const DailyDietPlan& dietPlan);//copy constructor
 ~DailyDietPlan();//destructor
 
@@ -47,6 +46,19 @@ public:
 DailyExercisePlan(int goalSteps = 0, std::string name = "Uknown", std::string date = "Uknown");//default constructor
 DailyExercisePlan(const DailyExercisePlan& exercisePlan);//copy constructor
 ~DailyExercisePlan();//destructor
+
+//setters
+bool setGoal(int goal);//returns true if valid goal ( > 0 ) is passed
+void setName(std::string name);
+void setDate(std::string date);
+//getters
+int getGoal();
+std::string getName();
+std::string getDate();
+
+//other member functions
+void printPlan();//prints outs plan for the day
+void editGoal();//prompts user for a new goal, loops until valid goal entered
 
 
 };
